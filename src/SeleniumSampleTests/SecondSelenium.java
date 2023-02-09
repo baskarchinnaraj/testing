2e8256d3791cee65ea1d0a174ffa3247f9ca2803
baskarchinnaraj <baskar.mca07.chinnaraj@gmail.com>
2023-02-09 21:40:51 +0530

    Create SecondSelenium.java
    Created Second SecondSelenium.java file and committed to master branch.

---
+++ src/SeleniumSampleTests/SecondSelenium.java
@@ -0,0 +1,11 @@
+package SeleniumSampleTests;
+
+public class SecondSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("helllo.");
+
+	}
+
+}
