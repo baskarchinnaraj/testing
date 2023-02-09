648228a9ae78ffc9214fe5d313bed80f1c00927f
Baskar Chinnaraj <baskar.chinnaraj.contractor@weather.com>
2023-02-09 21:56:21 +0530

    Pushing the ThirdSelenium java file to Brnach X

---
+++ src/SeleniumSampleTests/ThirdSelenium.java
@@ -0,0 +1,11 @@
+package SeleniumSampleTests;
+
+public class ThirdSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hi Hello..");
+
+	}
+
+}
