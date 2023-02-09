0c480a5d0b0a6ed5aef28c4be268865ed27b2547
Baskar Chinnaraj <baskar.chinnaraj.contractor@weather.com>
2023-02-09 20:13:27 +0530

    My Initial test files

---
+++ src/SeleniumSampleTests/FirstSelenium.java
@@ -0,0 +1,11 @@
+package SeleniumSampleTests;
+
+public class FirstSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hi");
+
+	}
+
+}
